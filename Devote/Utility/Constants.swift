6b36799b6b06b57e76a27d734e0725d5a62ef4c8
Maya Ghamloush <maya.ghamloushe@gmail.com>
2024-03-02 18:46:21 +0100

    Core Data Setup

---
+++ Devote/Utility/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  Devote
+//
+//  Created by Maya Ghamloush on 02/03/2024.
+//
+
+import Foundation
+//MARK: - FORMATTER
+
+ let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+//MARK: - UI
+
+//MARK: - UX
