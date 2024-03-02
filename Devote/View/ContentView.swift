030e4b74ed4472f93fbad3eefc62fc25b812251a
Maya Ghamloush <maya.ghamloushe@gmail.com>
2024-03-02 19:25:28 +0100

    Create A BAsic To Do App

--- Devote/View/ContentView.swift
+++ Devote/View/ContentView.swift
@@ -10,7 +10,10 @@ import CoreData
 
 struct ContentView: View {
     //MARK: - PROPERTIES
-    
+    @State var task: String = ""
+    private var isButtonDisabled: Bool{
+        task.isEmpty
+    }
     
     //MARK: - FETCHING DATA
     @Environment(\.managedObjectContext) private var viewContext
@@ -24,6 +27,9 @@ struct ContentView: View {
         withAnimation {
             let newItem = Item(context: viewContext)
             newItem.timestamp = Date()
+            newItem.task = task
+            newItem.completion = false
+            newItem.id = UUID()
 
             do {
                 try viewContext.save()
@@ -31,6 +37,8 @@ struct ContentView: View {
                  let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
+            task = ""
+            hideKeyboard()
         }
     }
 
@@ -41,8 +49,7 @@ struct ContentView: View {
             do {
                 try viewContext.save()
             } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+         
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
@@ -51,26 +58,57 @@ struct ContentView: View {
     //MARK: - BODY
     var body: some View {
         NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+            VStack {
+                VStack(spacing: 16){
+                    TextField("New Task", text: $task)
+                        .padding()
+                        .background(
+                            Color(UIColor.systemGray6)
+                        )
+                        .cornerRadius(10)
+                    
+                    Button(action: {
+                       addItem()
+                    }, label: {
+                        Spacer()
+                        Text("Save")
+                        Spacer()
+                    })
+                    .disabled(isButtonDisabled)
+                    .padding()
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .background(isButtonDisabled ? Color.gray : Color.pink)
+                    .cornerRadius(10)
+                }//VSTACK
+                .padding()
+                List {
+                    ForEach(items) { item in
+                        NavigationLink {
+                           
+                                
+                                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                                    .font(.footnote)
+                                    .foregroundColor(.gray)
+                         
+                        } label: {
+                            VStack(alignment: .leading) {
+                                Text(item.task ?? "")
+                                    .font(.headline)
+                                    .fontWeight(.bold)
+                                Text(item.timestamp!, formatter: itemFormatter)
+                            }}
                     }
-                }
-                .onDelete(perform: deleteItems)
-            }//List
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
+                    .onDelete(perform: deleteItems)
+                }//List
+                .navigationBarTitle("Daily Task", displayMode: .large)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        EditButton()
                     }
-                }
-            }
+                  
+            }//TOOLBAR
+            }//VSTACK
             Text("Select an item")
         }
     }
